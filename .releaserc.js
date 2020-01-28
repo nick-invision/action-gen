dc979367f41cfae75dcf61ef9a05247a8c6dd45b
Nick Fields <nickfields@invisionapp.com>
2020-01-28 10:38:42 -0500

    patch: added commitlint to enforce semantic-release commit format

--- .releaserc.js
+++ .releaserc.js
@@ -1,27 +1,17 @@
 module.exports = {
   plugins: [
     [
-      '@semantic-release/commit-analyzer',
       {
-        preset: 'angular',
         releaseRules: [
           { type: 'docs', scope: 'README', release: 'patch' },
-          { type: 'refactor', scope: 'core-*', release: 'minor' },
-          { type: 'refactor', release: 'patch' },
+          { type: 'minor', release: 'minor' },
+          { type: 'major', release: 'major' },
+          { type: 'patch', release: 'patch' },
           { scope: 'no-release', release: false },
         ],
       },
     ],
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/github',
-      {
-        assets: [
-          { path: 'dist/*', label: 'JS distribution' },
-          { path: 'Templates/*', label: 'Mustache Templates' },
-        ],
-      },
-    ],
   ],
   branch: 'master',
 };
